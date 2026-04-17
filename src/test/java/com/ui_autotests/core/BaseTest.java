7ec039ca8b5dfc27998f3a2db465b3ea1f343a4f
Artem Abaimov <artem.abaimov@inbox.ru>
2026-04-17 12:47:29 +0300

    create BasePage and BaseTest classes, connect dependencies

---
+++ src/test/java/com/ui_autotests/core/BaseTest.java
@@ -0,0 +1,36 @@
+package com.ui_autotests.core;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        BasePage.setDriver(driver);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+}
